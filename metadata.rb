fde30f35ef17dbb340603ef87026c228eb15ed25
Jason Blalock <jasonblalock@users.noreply.github.com>
2016-05-04 14:10:03 -0400

    Use ruby File methods

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures ruby_install'
 long_description 'Installs/Configures ruby_install'
-version '0.1.0'
+version '0.1.1'
 
 supports 'ubuntu', '>= 12.04'
 
--- providers/ruby.rb
+++ providers/ruby.rb
@@ -62,8 +62,8 @@ def install_dependencies
     command "apt-get update"
     ignore_failure true
     only_if do
-      !File.exists?('/var/lib/apt/periodic/update-success-stamp') ||
-      File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+      !::File.exists?('/var/lib/apt/periodic/update-success-stamp') ||
+      ::File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
     end
   end
 
