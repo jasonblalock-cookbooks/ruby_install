d560903f0ff08ae3b5c274310e1a981cf3964ee6
Jason Blalock <jasonblalock@users.noreply.github.com>
2017-03-14 10:59:25 -0400

    Update to ruby_install 0.6.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (C) 2016 Jason Blalock, All Rights Reserved
 
-default['ruby_install']['version'] = '0.6.0'
-default['ruby_install']['checksum'] = '3cc90846ca972d88b601789af2ad9ed0a496447a13cb986a3d74a4de062af37d'
+default['ruby_install']['version'] = '0.6.1'
+default['ruby_install']['checksum'] = 'b3adf199f8cd8f8d4a6176ab605db9ddd8521df8dbb2212f58f7b8273ed85e73'
 default['ruby_install']['url'] = "https://codeload.github.com/postmodern/ruby-install/tar.gz/v#{node['ruby_install']['version']}"
 default['ruby_install']['rubies'] = []
